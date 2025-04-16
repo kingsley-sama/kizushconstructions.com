8fd1628e730f65cf751e74a8b0fb6e7c2d7ea0e9
kingsley <kngsley2018@gmail.com>
2025-04-16 13:56:00 +0100

    blog page and contact us page

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import HomeSection from './pages/Homepage';
+import HomeSection, { Blogs } from './pages/Homepage';
 import AboutPage from './pages/aboutPage.jsx';
 import Services from './pages/services';
 import Projects from './pages/Projects';
@@ -18,6 +18,8 @@ const router = createBrowserRouter([
 			{ path: 'service', element: <Services /> },
 			{ path: 'project', element: <Projects /> },
 			{ path: 'contact', element: <Contact /> },
+			{ path: 'resource', element: <Blogs /> },
+
 		],
 	},
 ]);
