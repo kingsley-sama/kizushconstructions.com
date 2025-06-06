a073c0b56def6c37b94444c7c2c65508e59ec5c6
kingsley <kngsley2018@gmail.com>
2025-06-06 16:03:16 +0100

    db_data

--- server/db.py
+++ server/db.py
@@ -4,13 +4,8 @@ from sqlalchemy.orm import sessionmaker
 from dotenv import load_dotenv
 import os
 load_dotenv()
-DB_URL = "postgresql://{user}:{password}@{host}:{port}/{dbname}".format(
-    user=os.getenv('DB_USER'),
-    password=os.getenv('DB_PASSWORD'),
-    host=os.getenv('DB_HOST'),
-    port=os.getenv('DB_PORT'),
-    dbname=os.getenv('DB_NAME')
-)
+DB_URL = os.getenv("DB_URL")
+print(f"Connecting to database at {DB_URL}")
 engine = create_engine(DB_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
